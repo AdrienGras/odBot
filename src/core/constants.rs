66320f6bc2e15bb707adf7d6fd48c47d2513e2c1
Adrien Gras <agr@owlnext.fr>
2023-05-11 17:07:16 +0200

    wip

---
+++ src/core/constants.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+
+pub const ENV_RUST_LOG: &str = "RUST_LOG";
+pub const ENV_DISCORD_TOKENG: &str = "DISCORD_TOKEN";
+pub const ENV_CHANNEL_ID: &str = "CHANNEL_ID";
+pub const ENV_GUILD_ID: &str = "GUILD_ID";
---
+++ src/handlers/bot.rs
@@ -0,0 +1,13 @@
+use serenity::prelude::EventHandler;
+
+pub struct Bot {
+    // db
+}
+
+impl Bot {
+    pub fn new(/**db */) -> Self {
+        Self {  }
+    }
+}
+
+impl EventHandler for Bot {}
